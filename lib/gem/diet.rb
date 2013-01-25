5e5da7d597783c97c39b5a0167042421eade6ca6
Akira Matsuda <ronnie@dio.jp>
2013-01-25 16:33:35 +0900

    initial implementation

--- lib/gem/diet.rb
+++ lib/gem/diet.rb
@@ -1,7 +1,24 @@
-require "gem/diet/version"
+require 'gem/diet/version'
 
 module Gem
   module Diet
-    # Your code goes here...
+    def self.execute!
+      Gem.loaded_specs.each do |name, s|
+        s.author = s.date = s.description = s.email = s.homepage = s.rubyforge_project = s.summary = s.files = s.extra_rdoc_files = s.licenses = s.rdoc_options = nil
+      end
+    end
+  end
+end
+
+puts [*caller]
+
+if defined? Bundler
+  class Bundler::Runtime
+    def require_with_diet(*groups)
+      require(*groups)
+      Get::Diet.execute!
+    end
+    alias_method :require_without_diet, :require
+    alias_method :require, :require_with_diet
   end
 end
